bb7cdaf72564841ef7a3ce933157df45bd906fb5
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2022-06-24 14:52:15 +0200

    Create DIP.php

---
+++ Solid/DIP.php
@@ -0,0 +1,35 @@
+<?php
+
+class DataImporter
+{
+    private $loader;
+    private $gateway;
+
+    public function __construct(CsvFileLoader $csvFile, DataGateway $dataGateway)
+    {
+        
+    }
+}
+
+
+
+/* after *******************************************************************/
+
+interface CsvFileLoaderInterface
+{
+}
+
+interface DataGatewayInterface
+{
+}
+
+class DataImporter
+{
+    private $loader;
+    private $gateway;
+
+    public function __construct(CsvFileLoaderInterface $csvFile, DataGatewayInterface $dataGateway)
+    {
+        
+    }
+}
