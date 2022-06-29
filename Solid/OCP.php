d6261a2216b676c455f6709b8ab5a60ea3b1c3e9
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2022-06-29 16:11:05 +0200

    Update OCP.php

--- Solid/OCP.php
+++ Solid/OCP.php
@@ -1,14 +1,27 @@
 <?php
 
 class Speaker {
-    private $conversation = [
+    protected $conversation = [
       'make.it' => "You can do it",
       "success" => "Well done!"
     ];
   
-    public function speak($key)
-    {
+    public function speak($key) {
         return $this->conversation[$key];
     }
 }
 
+class SpeakerUpdated {
+     protected $conversation = [
+      'make.it' => "You can do it",
+      "success" => "Well done!"
+    ];
+    
+    public function setConversation($key, $newValue) {
+        $this->conversation[$key] = $newValue;
+    }
+  
+    public function speak($key) {
+        return $this->conversation[$key];
+    }
+}
