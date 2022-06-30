ca4627b02698998cd30c500555bc917c9be9efce
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2022-06-30 22:00:09 +0200

    Update LSP.php

--- Solid/LSP.php
+++ Solid/LSP.php
@@ -1,13 +1,26 @@
 <?php
 
-class Transaction{
-    public function buy(string $stock, int $quantity): object {
-        // ...
+class Price {
+    function getPrice(): int { return 0; }
+}
+class PromoPrice extends Price {
+    function getPrice(): int { return 1; }
+}
+
+class Transaction {
+    public function buy(string $stock, int $tarif): Price {
+        return new Price($tarif);
     }
 }
 
 class StockTransaction extends Transaction {
-    public function buy(string $stock, int $quantity): void {
-        // ...
+    public function buy(string $stock, int $tarif): PromoPrice {
+        return new PromoPrice($tarif);
+    }
+}
+
+class SubStockTransaction extends StockTransaction {
+    public function buy(string $stock, int $tarif): PromoPrice {
+        return new Price($tarif);
     }
 }
